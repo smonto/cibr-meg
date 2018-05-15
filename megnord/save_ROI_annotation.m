7793336cdb4116dd8b63fb46679fb893722f1d46
smonto <simo.monto@gmail.com>
2018-05-15 11:01:22 +0300

    Fixes to parcellation and annotations

--- megnord/save_ROI_annotation.m
+++ megnord/save_ROI_annotation.m
@@ -6,12 +6,19 @@ ct.struct_names=ROI.labels;
 
 % create a max-sparse parcel color grid in RGB space:
 n_div=floor(ROI.nROI^(1/3)); % how many points on every direction
-[X,Y,Z]=meshgrid(0:round(255/n_div):255); % include edges
+[X,Y,Z]=meshgrid([0:round(255/n_div):255 255]); % include edges
 ct.table=[X(:) Y(:) Z(:)];
 ct.table(:,4)=0;
 ct.table(:,5)=ct.table(:,1)+ct.table(:,2)*2^8+ct.table(:,3)*2^16;
+ct.table=ct.table(1:ROI.nROI,:);
 
-write_annotation([ROI.name '.annot'],0:ROI.n_sources-1,1:ROI.n_sources,ct)
+% deal annotation values to vertex labels
+verts=zeros(sum(ROI.n_sources),1);
+for nn=1:ROI.nROI
+    verts(ROI.ROIs{nn})=ct.table(nn,5);
+end
+
+write_annotation([ROI.name '.annot'],0:sum(ROI.n_sources)-1,verts,ct)
 
 saved=1;
 end
