f6a98a25411419dba9b42846e6030fcea201407b
Simo Monto Kärki6 Aa368.3 +358408053498 <sipemont@cibr-srv2.psy.jyu.fi>
2018-06-27 16:27:34 +0300

    Add downsampling to mf_raws

--- mf_raws.sh
+++ mf_raws.sh
@@ -3,7 +3,7 @@
 for k in *_raw.fif
 do
 echo "MaxFiltering $k..."
-nice /neuro/bin/util/maxfilter-3.0 -v -f $k -autobad on -origin fit -hpicons -st -movecomp inter -force > "MF_log_$k.txt"
+nice /neuro/bin/util/maxfilter-3.0 -v -f $k -autobad on -origin fit -hpicons -st -movecomp inter -ds 3 -force > "MF_log_$k.txt"
 done
 mkdir logs
 mv MF_log_*.txt logs/
