e23457095ce942c86531b350651e20b51f4f5469
smonto <simo.monto@gmail.com>
2018-11-28 13:10:22 +0200

    args

--- combine_fifs.py
+++ combine_fifs.py
@@ -7,25 +7,25 @@ from argparse import ArgumentParser
 #parse file names
 parser = ArgumentParser()
 parser.add_argument("combined_fname",nargs=1)
-parser.add_argument("orig_fnames",nargs="+")
+parser.add_argument("orig_fname",nargs="+")
 args = parser.parse_args()
 print("Combined filename: %s" % args.combined_fname[0])
-print("Original filenames: %s" % args.orig_fnames)
+print("Original filenames: %s" % args.orig_fname)
 combined_raw = list()
 
 # check infos match (channels)
-for fname in args.orig_fnames:
+for fname in args.orig_fname:
     raw_tmp = mne.io.read_raw_fif(fname, preload=False)
     combined_raw.append(raw_tmp)
     if not raw_tmp.info['ch_names'] == combined_raw[0].info['ch_names']:
-        sys.exit('Channel names in ' + fname + ' mismatch with ' + arg.orig_fnames[0])
+        sys.exit('Channel names in ' + fname + ' mismatch with ' + arg.orig_fname[0])
     if not raw_tmp.info['sfreq'] == combined_raw[0].info['sfreq']:
-        sys.exit('Sampling frequency in ' + fname + ' mismatches with ' + arg.orig_fnames[0])
+        sys.exit('Sampling frequency in ' + fname + ' mismatches with ' + arg.orig_fname[0])
     else:
         print('adding ' + fname  + ' to combined raw ' + combined_fname)
 
 # find new sampling frequency
-sfreq = combined_raw[0].info['sfreq'] / len(args.orig_fnames)
+sfreq = combined_raw[0].info['sfreq'] / len(args.orig_fname)
 
 # load data and resample
 for raw_tmp, idx in enumerate(combined_raw):
