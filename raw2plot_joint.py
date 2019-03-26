491139cdf5fc03084b269a1da67f388dbc0e1d01
smonto <simo.monto@gmail.com>
2019-03-26 10:48:16 +0200

    raw2plot_joint added

---
+++ raw2plot_joint.py
@@ -0,0 +1,14 @@
+# raw_to_evoked
+# simple utility to plot_joint from file name
+# ToDo: give file as argument
+
+raw_file='finger_elec_gnd_1.fif'
+event_id=1
+
+import mne
+Raw=mne.io.read_raw_fif(raw_file)
+Events=mne.find_events(Raw)
+Epochs=mne.Epochs(Raw, Events, event_id=event_id, tmin=-0.1, tmax=0.4,
+        baseline=(None, 0), proj=True)
+Evoked=Epochs.average()
+Evoked.plot_joint()
