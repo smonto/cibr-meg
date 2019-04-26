9371376bd1f34f7ac8c2f5dd74c3507ead30f6f5
smonto <simo.monto@gmail.com>
2019-04-26 16:07:01 +0300

    add reject to raw2plotjoint

--- raw2plot_joint.py
+++ raw2plot_joint.py
@@ -6,9 +6,11 @@ raw_file='finger_elec_gnd_1.fif'
 event_id=1
 
 import mne
+
 Raw=mne.io.read_raw_fif(raw_file)
 Events=mne.find_events(Raw)
+reject = dict(grad=4e-10, mag=4e-12, eog=150e-6)
 Epochs=mne.Epochs(Raw, Events, event_id=event_id, tmin=-0.1, tmax=0.4,
-        baseline=(None, 0), proj=True)
+        baseline=(None, 0), reject=reject, proj=True)
 Evoked=Epochs.average()
 Evoked.plot_joint()
