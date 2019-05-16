9c86b2b2cc6231e0c39c66cd2b94944a6376ade9
smonto <simo.monto@gmail.com>
2019-05-16 22:44:04 +0200

    mannheim commit

--- raw2ERF_TFR.py
+++ raw2ERF_TFR.py
@@ -4,11 +4,11 @@ event_id=1
 reject = dict(grad=4e-10, mag=4e-12, eog=150e-6)
 
 import mne
-import matplotlib.pyplot as plt
+from sys import argv
 from numpy import arange
 
-freqs = arange(4,51,2), # for TFR
-raw_file=sys.argv[1]
+freqs = arange(4,51,2) # for TFR
+raw_file=argv[1]
 ch_sets=[   'Vertex',
             'Left-temporal',
             'Right-temporal',
@@ -31,12 +31,9 @@ Epochs=mne.Epochs(Raw, Events, event_id=event_id, tmin=-0.4, tmax=1.2,
         baseline=(None, 0), reject=reject, proj=False)
 tf=mne.time_frequency.tfr_morlet(Epochs, freqs, n_cycles=freqs/2, n_jobs=8, decim=2, return_itc=False)
 tf=tf.crop(tmin=-0.2, tmax=1.0)
-nn=0
 for ch_set in ch_sets:
-    nn+=1
     ch_sel=mne.read_selection(ch_set, info=tf.info)
     picks=mne.pick_channels(tf.info['ch_names'],ch_sel)
-    plt.subplot(5,2,nn)
     tf.plot(mode='zscore', picks=picks, tmin=-0.3, tmax=1.0, baseline=(None,0), combine='mean',
-        title=raw_file + ch_set, show=True)
-plt.show()
+        title=raw_file[:-4] + ': ' + ch_set, show=True)
+print("Mission completed!")
---
+++ raw2PSD.py
@@ -0,0 +1,37 @@
+# simple utility to PSD for channel groups from raw
+import mne
+import matplotlib.pyplot as plt
+from sys import argv
+
+ch_type='grad' # 'grad' or 'mag'
+reject = dict(grad=4e-10, mag=4e-12, eog=150e-6)
+fmin, fmax = 2, 100
+n_fft = 2048
+
+raw_file=argv[1]
+ch_sets=[   'Left-temporal',
+            'Right-temporal',
+            'Left-parietal',
+            'Right-parietal',
+            'Left-occipital',
+            'Right-occipital',
+            'Left-frontal',
+            'Right-frontal',
+            'Vertex']
+
+Raw=mne.io.read_raw_fif(raw_file, preload=True)
+Raw.pick_types(meg=ch_type)
+plt.figure()
+nn=0
+for ch_set in ch_sets:
+    ch_sel=mne.read_selection(ch_set, info=Raw.info)
+    picks=mne.pick_channels(Raw.info['ch_names'],ch_sel)
+    nn+=1
+    ax=plt.subplot(5,2,nn)
+    ax.set_title(ch_set)
+    ax.set_xlim((0,100))
+    Raw.plot_psd(fmin=fmin, fmax=fmax, n_fft=n_fft, ax=ax,
+                 proj=False, picks=picks, area_mode='std',
+                 show=False, average=True)
+plt.show(block=False)
+print("Mission completed!")
