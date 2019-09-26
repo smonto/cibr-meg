a542d1f8cd3d1fcc3293fc5f533de55c22fd663b
Simo Monto <sipemont@jyu.fi>
2019-09-26 15:17:01 +0300

    save

---
+++ raw2plot.py
@@ -0,0 +1,23 @@
+# raw data plot
+# simple utility to plot raw file from file name
+import sys
+import mne
+
+raw_file=sys.argv[1]
+sfreq=sys.argv[2]
+ch_type=sys.argv[3]
+print(ch_type)
+print(type(sys.argv[3]))
+print(len(sys.argv))
+print(len(sys.argv[3]))
+
+kwargs=dict()
+for kw in sys.argv[3:]:
+#    kwargs={sys.argv[3]:True}
+    kwargs=kwargs.update({kw:True})
+
+Raw=mne.io.read_raw_fif(raw_file, preload=True)
+Raw.resample(sfreq)
+Raw.pick_types(Raw.info, **kwargs)
+Raw.plot()
+# end
