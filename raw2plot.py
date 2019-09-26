a8cbc07eae9352f1a0ce16093995ebc8d9067eb9
smonto <simo.monto@gmail.com>
2019-09-26 12:01:18 +0300

    södiurgher

--- raw2plot.py
+++ raw2plot.py
@@ -4,9 +4,12 @@
 import sys
 import mne
 
+len(sys.argv)
 raw_file=sys.argv[1]
 sfreq=sys.argv[2]
 ch_type=sys.argv[3]
+print(ch_type)
+print(type(sys.argv[3]))
 
 Raw=mne.io.read_raw_fif(raw_file, preload=True)
 Raw.resample(sfreq)
