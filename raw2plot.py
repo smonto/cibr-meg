1b122829342d50ca9056e601cb333acb7f2ba086
smonto <simo.monto@gmail.com>
2019-09-26 11:44:05 +0300

    mod raw2plot

--- raw2plot.py
+++ raw2plot.py
@@ -7,6 +7,6 @@ import mne
 raw_file=sys.argv[1]
 ch_type=sys.argv[2]
 
-Raw=mne.io.read_raw_fif(raw_file)
+Raw=mne.io.read_raw_fif(raw_file, preload=True)
 Raw.pick_types(Raw.info, ch_type=True)
 Raw.plot()
