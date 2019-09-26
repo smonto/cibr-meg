880e66aff1b714bf8acb0bb9d664ed8504304daf
smonto <simo.monto@gmail.com>
2019-09-26 13:49:13 +0300

    one more try

--- raw2plot.py
+++ raw2plot.py
@@ -4,14 +4,17 @@
 import sys
 import mne
 
-len(sys.argv)
 raw_file=sys.argv[1]
 sfreq=sys.argv[2]
 ch_type=sys.argv[3]
 print(ch_type)
 print(type(sys.argv[3]))
+print(len(sys.argv))
+print(len(sys.argv[3]))
+
+kwargs={sys.argv[3]):True}
 
 Raw=mne.io.read_raw_fif(raw_file, preload=True)
 Raw.resample(sfreq)
-Raw.pick_types(Raw.info, ch_type=True)
+Raw.pick_types(Raw.info, **kwargs)
 Raw.plot()
