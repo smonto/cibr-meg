2c2d82b264a6ba0782fc0cca7bd1306358dc346c
smonto <simo.monto@gmail.com>
2019-10-22 15:32:28 +0300

    files combine

--- maxfilter/mf_raws_transmean.sh
+++ maxfilter/mf_raws_transmean.sh
@@ -5,9 +5,10 @@
 # NOTE! replace "id" and "project" with correct names!
 # NOTE! edit the MaxFilter command according to need
 
-id="elpis15"
-project="elpis"
-file_list=($(find /projects/${project}/testmf/ -iname "*${id}*.fif"))
+id="subject"
+project="project"
+file_list=($(find /projects/${project}/orig/ -iname "*${id}*.fif"))
+combined_file_name="dummy.fif"
 
 # STEP1: run maxfilter -headpos to get .pos files
 for RAWFILE in ${file_list[@]}
@@ -16,19 +17,25 @@ for RAWFILE in ${file_list[@]}
         mkdir -p /projects/${project}/maxfiltered/${id}
         NOPATH=$(basename $RAWFILE)
         OUTNAME=${NOPATH%%.fif}
-        nice /neuro/bin/util/maxfilter-3.0 -headpos -frame head -o /projects/${project}/maxfiltered/${id}/${OUTNAME}_tsss_quat.fif -v -f $RAWFILE -autobad on -origin fit -hpicons -st -force
+        nice /neuro/bin/util/maxfilter-3.0 -headpos -frame head -o /projects/${project}/maxfiltered/${id}/${OUTNAME}_tsss_quat.fif -f $RAWFILE -autobad on -origin fit -hpicons -st -force
     done
 
 # STEP2: run mean_meg2head to obtain mean head position over runs
-python /opt/tools/cibr-meg/maxfilter/mean_meg2head_trans.py /projects/${project}/maxfiltered/${id}
+/opt/anaconda/bin/python /opt/tools/cibr-meg/maxfilter/mean_meg2head_trans.py /projects/${project}/maxfiltered/${id}
 
 # STEP3: run MaxFilter to clean data and transform to mean head position
 mkdir -p /projects/${project}/maxfiltered/logs
+n=0
 for RAWFILE in ${file_list[@]}
     do
         echo "MaxFiltering $RAWFILE..."
         NOPATH=$(basename $RAWFILE)
         OUTNAME=${NOPATH%%.fif}
-        nice /neuro/bin/util/maxfilter-3.0 -movecomp inter -autobad on -frame head -o /projects/${project}/maxfiltered/${id}/${OUTNAME}_tsss_mc.fif -trans /projects/${project}/maxfiltered/${id}/mean-trans.fif -v -f $RAWFILE -origin fit -hpicons -st -ds 4 -force | tee /projects/${project}/maxfiltered/logs/${OUTNAME}.log
+        nice /neuro/bin/util/maxfilter-3.0 -movecomp inter -autobad on -frame head -o /projects/${project}/maxfiltered/${id}/${OUTNAME}_tsss_mc.fif -trans /projects/${project}/maxfiltered/${id}/mean-trans.fif -f $RAWFILE -origin fit -hpicons -st -ds 4 -force | tee /projects/${project}/maxfiltered/logs/${OUTNAME}.log
+        mf_list[n]=/projects/${project}/maxfiltered/${id}/${OUTNAME}_tsss_mc.fif
+        n=n+1
     done
 echo "Maxfiltering ready!"
+
+# STEP4: combine resulting Maxfiltered files
+/opt/anaconda/bin/python /opt/tools/cibr-meg/combine_fifs.py combined_file_name "${mf_list[@]}"
