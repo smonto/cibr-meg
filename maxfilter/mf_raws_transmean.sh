c7e15eefb116479bf53540d18bd0e7da477ef7c7
smonto <simo.monto@gmail.com>
2019-10-22 12:50:57 +0300

    mf_raws_transmean.sh 1st commit

---
+++ maxfilter/mf_raws_transmean.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# This script performs Maxfiltering for given subject, with
+# head position transformed to the mean over all runs with this id
+# NOTE! replace "id" and "project" with correct names!
+# NOTE! edit the MaxFilter command according to need
+
+id="01"
+project="meg"
+file_list=($(find /projects/${project}/orig/ -name "*${id}*.fif"))
+
+# STEP1: run maxfilter -headpos to get .pos files
+for RAWFILE in ${file_list[@]}
+    do
+        echo "Finding head movements in $RAWFILE..."
+        mkdir -p /projects/${project}/maxfiltered/${id}
+        NOPATH=$(basename $RAWFILE)
+        OUTNAME=${NOPATH%%.fif}
+        nice /neuro/bin/util/maxfilter-3.0 -headpos -frame head -o /projects/${project}/maxfiltered/${id}/${OUTNAME}_tsss_quat.fif -v -f $RAWFILE -autobad on -origin fit -hpicons -st -force
+    done
+
+# STEP2: run mean_meg2head to obtain mean head position over runs
+python mean_meg2head_trans.py /projects/${project}/maxfiltered/${id}
+
+# STEP3: run MaxFilter to clean data and transform to mean head position
+mkdir -p /projects/${project}/maxfiltered/logs
+for RAWFILE in ${file_list[@]}
+    do
+        echo "MaxFiltering $RAWFILE..."
+        NOPATH=$(basename $RAWFILE)
+        OUTNAME=${NOPATH%%.fif}
+        nice /neuro/bin/util/maxfilter-3.0 -movecomp inter -autobad on -frame head -o /projects/${project}/maxfiltered/${id}/${OUTNAME}_tsss_mc.fif -trans /projects/${project}/maxfiltered/${id}/mean-trans.fif -v -f $RAWFILE -origin fit -hpicons -st -ds 4 -force | tee /projects/${project}/maxfiltered/logs/${OUTNAME}.log
+    done
+echo "Maxfiltering ready!"
