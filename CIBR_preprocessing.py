16a6268ac3a29d702960580b3d28cdcba6d84540
Simo Monto <simo@dyn-130-234-243-108.vpn.jyu.fi>
2020-09-08 11:14:02 +0300

    copied Logger object def

--- CIBR_preprocessing.py
+++ CIBR_preprocessing.py
@@ -51,6 +51,23 @@ import sys
 sys.path.append("/opt/tools/cibr-meg/")
 import compare_raws
 
+"""
+from stackexchange, should we need this:
+class Logger(object):
+    def __init__(self):
+        self.terminal = sys.stdout
+        self.log = open("logfile.log", "a")
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+    def flush(self):
+        #this flush method is needed for python 3 compatibility.
+        #this handles the flush command by doing nothing.
+        #you might want to specify some extra behavior here.
+        pass    
+sys.stdout = Logger()
+"""
+
 # CIBR cross-talk correction and calibration files for MaxFilter:
 ctc = '/neuro/databases/ctc/ct_sparse.fif'
 cal = '/neuro/databases/sss/sss_cal.dat'
